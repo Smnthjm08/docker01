26155506bf25831fb45ebab2d626bed953cb09d5
Smnthjm08 <sumanthjmm.04@gmail.com>
2024-11-21 23:45:41 +0530

    chore: client setup with shadcn

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,3 @@
-
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 
@@ -8,24 +7,36 @@ app.use(express.json());
 const client = new PrismaClient();
 
 app.get("/", (req, res) => {
-    res.json({
-        message: "Healthy server"
-    })
-})
+  res.json({
+    message: "Healthy server",
+  });
+});
+
+app.get("/get", (req, res) => {
+  res.json({
+    message: "Hello Broo",
+  });
+});
+
+app.get("/get/new", (req, res) => {
+  res.json({
+    message: "Hello Broo",
+  });
+});
 
 app.post("/", async (req, res) => {
-    await client.user.create({
-        data: {
-            email: req.body.email,
-            name: req.body.name
-        }
-    })
+  await client.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+    },
+  });
 
-    res.json({
-        message: "Done signing up!"
-    })
-})
+  res.json({
+    message: "Done signing up!",
+  });
+});
 
-app.listen(3000,()=>{
-     console.log(`Server is running on port ${3000}`)
+app.listen(3000, () => {
+  console.log(`Server is running on port ${3000}`);
 });
